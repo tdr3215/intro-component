f224159797ee3396a4d75df9a57f2fb40c8768dd
Tiffany <tiffany.drose@gmail.com>
2021-06-17 16:35:48 -0400

    added a js file as well as a comment for the error utility classes in the css

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* recieve error message upon submit 
+--Any `input` field is empty. The message for this error 
+should say *"[Field Name] cannot be empty"*
+ --The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error
+should say *"Looks like this is not an email"*
+*/
