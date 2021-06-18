3abe35683bf70834029cfc43284b000b0288f660
Tiffany <tiffany.drose@gmail.com>
2021-06-18 15:27:27 -0400

    working on the javascript component and and the error states so adjusting app.css and index.html accordingly

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 'use strict';
 
 /* recieve error message upon submit 
+----Activate error class only when the following occurs
+----Replace each placeholder text with an empty string except for the email
 --Any `input` field is empty. The message for this error 
 should say *"[Field Name] cannot be empty"*
  --The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error
 should say *"Looks like this is not an email"*
 */
+
+// Looping through the inputs/adding a span element when the error occurs
+const input = document.querySelectorAll('input');
+
+
+
+
+
+
+// Adding the error spans to the page
+//  <em><span id="error-span">First Name cannot be empty</span></em>
