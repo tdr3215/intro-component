651824d02d1321e8d4f79eaad3f165cf46450dbf
Tiffany <tiffany.drose@gmail.com>
2021-06-21 18:19:58 -0400

    slowly working towards adding the js component for the alerts, need to add a span when something goes wrong instead of having them prepositioned

--- app.js
+++ app.js
@@ -9,15 +9,60 @@ should say *"[Field Name] cannot be empty"*
 should say *"Looks like this is not an email"*
 */
 
-// Looping through the inputs/adding a span element when the error occurs
-const input = document.querySelectorAll('input');
+
+
+// Looping through the inputs/make display flex when error occurs
+const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
+const submitButton = document.querySelector('button');
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+const firstNameId = document.getElementById('first-name');
+const lastNameId = document.getElementById('last-name');
+const emailId = document.getElementById('email');
+const passwordId = document.getElementById('password');
+const errorSpan = document.querySelector('.error-span');
+
+form.addEventListener('submit', function(){
+
+for(let input of inputs){
+
+    //give each span in that section the display of error-span
+
+if(firstName.value.length == 0){
+    errorSpan.style.display = 'flex'; //try pushing the span into the div if all else fails
+}else if(lastName.value.length == 0) {
+    errorSpan.style.display = 'flex';
+}else if(email.value.length == 0){
+    errorSpan.style.display = 'flex';
+}else if(password.value.length == 0){
+    errorSpan.style.display = 'flex';
+ }
+}
+
+})
+
+
 
 
 //for loop inside of a submit event listener
 
 
 
-// Adding the error spans/div to the page
+/*Adding the error spans/div to the page(set #error-span to flex and 
+add the error class to the div)
+error-class==> info-section,input-card
+
+
+
+*/
+
+
+
+
+
 /*
 <div>
 
